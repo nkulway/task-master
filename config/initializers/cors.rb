769f48836d7808c44715d160e6ffae437eccfa4d
nkulway <nkulway@gmail.com>
2025-01-18 16:21:28 -0500

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins ENV["CORS_ALLOWED_ORIGIN"] || "http://localhost:4200"
     resource "*",
-      headers: :any,
+      headers: [ "Content-Type", "Authorization" ],
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
